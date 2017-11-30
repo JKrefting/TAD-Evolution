dc954babdab9d9c515b180553313d6448b89374b
ibn-salem <jonas.ibn-salem@gmx.de>
2017-11-30 16:55:54 +0100

    adjust filtering to new data tables

--- R/ortholog_expression_plots.R
+++ R/ortholog_expression_plots.R
@@ -3,27 +3,33 @@ require(RColorBrewer)
 require(ggsignif)
 
 # data <- read_rds("results/ortholog_expression_correlation_old.rds")
-data <- read_rds("results/ortholog_expression_correlation.rds")
+# data <- read_rds("results/ortholog_expression_correlation.rds")
+genes_by_domain_categories <- read_rds("results/genes_by_domain_categories.rds")
+
+data <- genes_by_domain_categories %>% 
+  filter(species == "mm10" | is.na(species)) %>% 
+  filter(domain_type == "hESC")
+
 
 data_stats <- data %>%
-  group_by(domain_type, category) %>%
+  group_by(domain_type, gene_category) %>%
   summarise(count = n(), 
             n_tads = length(unique(domain_id)))
 
 plot_data <- data %>% 
   filter(
     domain_type == "hESC", 
-    !is.na(category), 
+    !is.na(gene_category), 
     !is.na(correlation)
     )
 
 # my_palette = c(brewer.pal(5, "Dark2")[2], brewer.pal(5, "Set1")[c(2, 1, 3)])
 my_palette = brewer.pal(5, "Set1")[c(2, 1, 3)]
 # Plot correlations seperately depending on class
-ggplot(plot_data, aes(x = category, y = correlation)) + 
-    geom_violin(aes(color = category, fill = category), adjust = 0.25, alpha = 0.3) +
-    geom_boxplot(aes(color = category), outlier.size = 0.3, width = 0.2) + 
-    geom_signif(comparisons=list(
+ggplot(plot_data, aes(x = gene_category, y = correlation)) + 
+    geom_violin(aes(color = gene_category, fill = gene_category), adjust = 0.25, alpha = 0.3) +
+    geom_boxplot(aes(color = gene_category), outlier.size = 0.3, width = 0.2) + 
+    geom_signif(comparisons = list(
       c("Conserved", "Rearranged"),
       c("Conserved", "Outside")), 
       map_signif_level = FALSE,
