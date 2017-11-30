d0a1fe002d9210220d4e693df33436922b067798
ibn-salem <jonas.ibn-salem@gmx.de>
2017-11-30 18:59:23 +0100

    update boxplot and add summary statistics as table

--- R/ortholog_expression_plots.R
+++ R/ortholog_expression_plots.R
@@ -2,69 +2,83 @@ require(tidyverse)
 require(RColorBrewer)
 require(ggsignif)
 
-# data <- read_rds("results/ortholog_expression_correlation_old.rds")
-# data <- read_rds("results/ortholog_expression_correlation.rds")
+# parameters
+
+# my_palette = c(brewer.pal(5, "Dark2")[2], brewer.pal(5, "Set1")[c(2, 1, 3)])
+GENE_CATEGORY_COLORS = brewer.pal(5, "Set1")[c(2, 1, 3)]
+
+#*******************************************************************************
+# read table with genes, expression correlation, domains, and categories
+#*******************************************************************************
 genes_by_domain_categories <- read_rds("results/genes_by_domain_categories.rds")
 
+
+#*******************************************************************************
+# count categories per species and domain type
+#*******************************************************************************
+count_df <- tidy_domain_groups %>% 
+  count(species, domain_type, conserved, rearranged, category) %>% 
+  write_tsv("results/tidy_domain_groups.species_domain_rearraged_conserved.counts.tsv")
+
+count_df <- tidy_domain_groups %>% 
+  count(species, domain_type, category) %>% 
+  write_tsv("results/tidy_domain_groups.species_domain_category.counts.tsv")
+
+#*******************************************************************************
+# Analyse correlation by TAD category for mouse in hESC
+#*******************************************************************************
+
+# filter for only mouse data and hESC TADs
 data <- genes_by_domain_categories %>% 
   filter(species == "mm10" | is.na(species)) %>% 
   filter(domain_type == "hESC")
 
-
-data_stats <- data %>%
-  group_by(domain_type, gene_category) %>%
-  summarise(count = n(), 
-            n_tads = length(unique(domain_id)))
+# summarize statistics on gene level
+summaryDF <- data %>%
+  group_by(gene_category) %>%
+  summarise(
+    n_TADs_with_gene = length(unique(domain_id)),
+    n_genes = n(), 
+    n_genes_with_cor = sum(!is.na(correlation)),
+    cor_mean = mean(correlation, na.rm = TRUE),
+    cor_median = median(correlation, na.rm = TRUE),
+    cor_sd = sd(correlation, na.rm = TRUE)
+    ) %>% 
+  write_tsv("results/ortholog_expression.mm10.TAD_hESC.summaryDF.tsv")
 
 plot_data <- data %>% 
   filter(
-    domain_type == "hESC", 
-    !is.na(gene_category), 
+    !is.na(gene_category),
     !is.na(correlation)
+    ) %>% 
+  mutate(gene_category = factor(
+    gene_category, 
+    levels = c("Conserved", "Rearranged", "Outside"),
+    labels = c("Conserved TAD", "Rearranged TAD", "Outside TAD"))
     )
 
-# my_palette = c(brewer.pal(5, "Dark2")[2], brewer.pal(5, "Set1")[c(2, 1, 3)])
-my_palette = brewer.pal(5, "Set1")[c(2, 1, 3)]
-# Plot correlations seperately depending on class
-ggplot(plot_data, aes(x = gene_category, y = correlation)) + 
-    geom_violin(aes(color = gene_category, fill = gene_category), adjust = 0.25, alpha = 0.3) +
-    geom_boxplot(aes(color = gene_category), outlier.size = 0.3, width = 0.2) + 
-    geom_signif(comparisons = list(
-      c("Conserved", "Rearranged"),
-      c("Conserved", "Outside")), 
-      map_signif_level = FALSE,
-      test = wilcox.test,
-      textsize = 3,
-      tip_length = 0,
-      y_position = c(1.05, 1.2, 1.35, 1.5),
-      step_increase = 0.13) + 
-    scale_color_manual("", labels = c(
-      "Conserved TAD", "Rearranged TAD", "Outside TAD"),
-      values = my_palette
-    ) +
-    scale_fill_manual("", labels = c(
-      "Conserved TAD", "Rearranged TAD", "Outside TAD"),
-      values = my_palette
-    ) +
-    theme_minimal() + 
-    theme(plot.title = element_text(size=12, face = "bold", colour = "black"),
-          legend.text = element_text(size = 10, face = "bold"),
-          legend.position = "bottom",
-          # legend.position = c(0.5, -0.03),
-          legend.direction = "horizontal",
-          legend.key.width = unit(0.3, "cm"),
-          legend.key.height = unit(0.3, "cm"),
-          strip.background = element_blank(),
-          strip.text = element_text(size = 10, face = "bold"),
-          axis.title.x = element_text(face="bold", size=10),
-          axis.title.y = element_text(face="bold", size=10),
-          axis.text.x = element_blank(), 
-          # axis.text.x = element_text(face="bold", size=10),
-          axis.text.y = element_text(face="bold", size=10),
-          axis.ticks.x = element_blank(),
-          panel.grid.major = element_blank(),
-          # panel.grid.minor = element_blank(),
-          plot.margin=unit(c(0.1,0.5,0,0.5), "cm")) +
-    ylim(c(-1, 1.35)) + 
-    xlab("") + 
-    ylab("Ortholog expression correlation")
+
+p <- ggplot(plot_data, aes(x = gene_category, y = correlation, color = gene_category, fill = gene_category)) + 
+  geom_violin(color = NA, adjust = 0.25, alpha = 0.3) +
+  geom_boxplot(fill = "white", outlier.size = 0.3, width = 0.2) + 
+  geom_signif(comparisons = list(
+    c("Conserved TAD", "Rearranged TAD"),
+    c("Conserved TAD", "Outside TAD")), 
+    color = "black",
+    map_signif_level = FALSE,
+    test = wilcox.test,
+    tip_length = 0,
+    y_position = c(1.05, 1.2, 1.35, 1.5),
+    step_increase = 0.13) +
+  scale_color_manual(values = GENE_CATEGORY_COLORS) + 
+  scale_fill_manual(values = GENE_CATEGORY_COLORS) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        # text = element_text(size = 20),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ylim(-.5, 1.3) +
+  labs(x = "", y = "Ortholog expression\ncorrelation")
+
+ggsave("results/ortholog_expression.mm10.TAD_hESC.cor_by_gene_category.boxplot.pdf",
+       h = 3, w = 3)
+
