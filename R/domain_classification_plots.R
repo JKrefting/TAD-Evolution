69974c5b23ab8864cd3299daf22d66357da6d6c9
ibn-salem <jonas.ibn-salem@gmx.de>
2017-12-07 15:44:21 +0100

    count gene categories and GRB TADs

--- R/domain_classification_plots.R
+++ R/domain_classification_plots.R
@@ -13,6 +13,7 @@ TAD_CATEGORY_COLORS = brewer.pal(9, "Set1")[c(2, 1, 9)]
 #*******************************************************************************
 tidy_domain_groups <- read_rds("results/tidy_domain_groups.rds")
 
+domain_to_GRB <- read_rds("results/domain_to_GRB.rds")
 
 #*******************************************************************************
 # count categories per species and domain type
@@ -24,6 +25,29 @@ count_df <- tidy_domain_groups %>%
 count_df <- tidy_domain_groups %>% 
   count(species, domain_type, category) %>% 
   write_tsv("results/tidy_domain_groups.species_domain_category.counts.tsv")
+#*******************************************************************************
+# count gene categories and GRB-TADs
+#*******************************************************************************
+TADs_mm10_hESC <- tidy_domain_groups %>% 
+  filter(
+    species == "mm10" | is.na(species),
+    domain_type == "hESC"
+    ) %>% 
+  left_join(domain_to_GRB, by = c("domain_id", "domain_type"))
+  
+TADs_mm10_hESC_category <- TADs_mm10_hESC %>% 
+  count(category) %>%
+  mutate(
+    percent = n / sum(n) * 100
+  ) %>% 
+  write_tsv("results/tidy_domain_groups.mm10_hESC.category.counts.tsv")
+
+TADs_mm10_hESC_GRB_class <- TADs_mm10_hESC %>%
+  count(GRB_class) %>%
+  mutate(
+    percent = n / sum(n) * 100
+  ) %>% 
+  write_tsv("results/tidy_domain_groups.mm10_hESC.GRB_class.counts.tsv")
 
 #*******************************************************************************
 # Plot categorization of domains in classes
