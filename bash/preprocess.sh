a61defdc5078b685086f9b8e6a6043d9c9d0f03e
ibn-salem <jonas.ibn-salem@gmx.de>
2018-06-21 23:49:22 +0200

    Fix issue with empty breakpoint file

--- bash/preprocess.sh
+++ bash/preprocess.sh
@@ -17,25 +17,33 @@ SPECIES="$(cut -f1 species_meta.tsv | tail -n +2)"
 THRESHOLDS="$(cut -f1 metadata.tsv | tail -n +2)"
 
 for S in $SPECIES ; do
-	
-	# Extract rearrangement breakpoints from alignments
-	for T in $THRESHOLDS ; do
-	
-			python Python/find_breakpoints.py \
-			-i 'data/alignments/'hg38.$S.net \
-			-o 'data/breakpoints/'hg38.$S.$T.bp.bed \
-			-t $T
-			
-			# filter for plain chromosomes
-      cat data/breakpoints/hg38.${S}.${T}.bp.bed \
-        | egrep -v "(KI|GL|JH|KB)" \
-        > data/breakpoints/hg38.$S.$T.bp.flt.bed
 
-	done
+    echo "INFO: "$S" -----------------------------------------------------------#"
   
-  # get the coordinates of syntenic alignment blocks (fills)
-  python Python/get_fills.py  \
-    -i data/alignments/hg38.$S.net \
-    -o data/fills/hg38.$S.fill.bed
-
+    # get the coordinates of syntenic alignment blocks (fills)
+    python Python/get_fills.py  \
+        -i data/alignments/hg38.$S.net \
+        -o data/fills/hg38.$S.fill.bed
+    
+    for T in $THRESHOLDS ; do
+    
+        # Extract rearrangement breakpoints from alignments
+        python Python/find_breakpoints.py \
+            -i 'data/alignments/'hg38.${S}.net \
+            -o 'data/breakpoints/'hg38.${S}.${T}.bp.bed \
+            -t ${T}
+        
+        # check if file is present and non-empty    
+        if [ -s data/breakpoints/hg38.${S}.${T}.bp.be ]
+        then
+            # filter for plain chromosomes
+            cat data/breakpoints/hg38.${S}.${T}.bp.bed \
+              | egrep -v "(KI|GL|JH|KB)" \
+              > data/breakpoints/hg38.${S}.${T}.bp.flt.bed
+        else
+            # create emty file
+            touch data/breakpoints/hg38.${S}.${T}.bp.flt.bed
+        fi
+            
+    done
 done
